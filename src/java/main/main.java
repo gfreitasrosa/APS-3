70280cca0c364f8e132ad21eb2a270209d27415a
Gabriel Rosa <gfreitasrosa27@gmail.com>
2022-05-09 20:38:15 -0300

    add main

---
+++ src/java/main/main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class main {
+    public static void main(String[] args) {
+
+        String alunoDir = "alunos.csv";
+        String cursoDir = "cursos.csv";
+        String matriculaDir = "matricula.csv";
+    }
+}
