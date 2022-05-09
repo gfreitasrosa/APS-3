bf4f4f196fc9b3f14ab64df75b87b04ae6c20453
Pedro Alquimim <89994181+pedrOAlquimim@users.noreply.github.com>
2022-05-09 20:02:16 -0300

    Method saveAlunos() finished

--- src/java/csv/ManipularCsv.java
+++ src/java/csv/ManipularCsv.java
@@ -18,8 +18,21 @@ import entidades.Curso;
 import entidades.Matricula;
 
 public class ManipularCsv {
+  private String saveAluno;
 
-  private void saveFuncionarios(){
+  private void saveAlunos(Set<Aluno> alunoOutput){
+    try (
+      OutputStream outputSream = new FileOutputStream(this.saveAluno);
+      OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputSream, StandardCharsets.UTF_8);
+      PrintWriter printWriter = new PrintWriter(outputStreamWriter, true);
+      ){
+          for (Aluno aluno : alunoOutput) {
+            printWriter.println(aluno.getId() + "," + aluno.getNome());
+          }
+
+    } catch(IOException error){
+      error.printStackTrace();
+    }
   }
 
 }
